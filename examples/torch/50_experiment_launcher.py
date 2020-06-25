c3ff6d6d516b2ad392d52754328b235e31670d59
avnishn <avnishna@usc.edu>
2020-06-25 11:32:51 -0700

    Temp

--- examples/torch/50_experiment_launcher.py
+++ examples/torch/50_experiment_launcher.py
@@ -23,5 +23,5 @@ for i, name in enumerate(names):
     gpu_id = i % 4
     conda_args = ['source ~/miniconda3/etc/profile.d/conda.sh; conda activate garage; python ~/garage/examples/torch/sac_metaworldv2_test_max_path_length_1000.py --gpu {} --env {} && python -v'.format(gpu_id, name)]
     FNULL = open(os.devnull, 'w')
-    p = subprocess.Popen(conda_args, shell=True, executable="/bin/bash", stdout=FNULL, stderr=subprocess.STDOUT)
+    p = subprocess.Popen(conda_args, shell=True, executable='/bin/bash', stdout=FNULL, stderr=subprocess.STDOUT)
     # p = subprocess.Popen(conda_args, shell=True, executable="/bin/bash",)
