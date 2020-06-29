104c8d3f64cf57e41d128ca3adb8a9d48287edec
irisliucy <irisliu.0616@gmail.com>
2020-06-28 20:54:34 -0700

    Add vanilla gru

--- src/garage/torch/modules/__init__.py
+++ src/garage/torch/modules/__init__.py
@@ -3,6 +3,7 @@
 from garage.torch.modules.gaussian_mlp_module import \
     GaussianMLPIndependentStdModule, GaussianMLPModule, \
     GaussianMLPTwoHeadedModule
+from garage.torch.modules.gru_module import GRUModule
 from garage.torch.modules.mlp_module import MLPModule
 from garage.torch.modules.multi_headed_mlp_module import MultiHeadedMLPModule
 
@@ -12,4 +13,5 @@ __all__ = [
     'GaussianMLPModule',
     'GaussianMLPIndependentStdModule',
     'GaussianMLPTwoHeadedModule',
+    'GRUModule',
 ]
